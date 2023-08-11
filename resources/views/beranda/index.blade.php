13d2a000f84480b77ac262d6bcfec18011a83751
ivanverdyansyah07 <ivanverdyansyah11@gmail.com>
2023-08-12 04:25:50 +0800

    Progress: Edit Beranda Section Opening

--- resources/views/beranda/index.blade.php
+++ resources/views/beranda/index.blade.php
@@ -360,11 +360,6 @@
     </div>
     {{-- END MODAL EDIT SECTION OPENING --}}
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
-    </script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-
     <script>
         $(document).on('click', '[data-bs-target="#detailSectionHeaderModal"]', function() {
             $.ajax({
--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -12,6 +12,14 @@
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     {{-- END STYLE CSS --}}
 
+
+    {{-- SCRIPT JS --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
+    </script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    {{-- END SCRIPT JS --}}
+
 </head>
 
 <body>
