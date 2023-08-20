5223f889e5b687760e9bdb089a8eec336c248f72
ivanverdyansyah07 <ivanverdyansyah11@gmail.com>
2023-08-20 09:23:16 +0800

    Progress: Add Pagination on Manajemen Profil Page

--- resources/views/profil/manajemen/index.blade.php
+++ resources/views/profil/manajemen/index.blade.php
@@ -112,6 +112,9 @@
                     @endif
                 </div>
             </div>
+            <div class="col-12 d-flex justify-content-end mt-4">
+                {{ $managements->links() }}
+            </div>
         </div>
     </div>
 
